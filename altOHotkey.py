ef83ef588f486a8e77b56e8ae2c19ad4bcacf592
Andreas George Nechita <ga.nechita@gmail.com>
2024-06-02 23:15:58 +0200

    simplified code

--- altOHotkey.py
+++ altOHotkey.py
@@ -1,28 +1,8 @@
 from pynput import keyboard as pynputKeyboard
 import tkinterGUI
+import keyboard
 
-print('Ready to start using ALT+O Hotkey')
 
-def on_press(key):
-    if key == pynputKeyboard.Key.alt_l:
-        global alt_pressed
-        alt_pressed = True
-    elif hasattr(key, 'char') and key.char == 'o':
-        if alt_pressed:
-            # Add your desired action here
-            # runFile.py_file("tkinterGUI.py")
-            tkinterGUI.run()
-def on_release(key):
-    global alt_pressed
-    if key == pynputKeyboard.Key.alt_l:
-        alt_pressed = False
-    elif key == pynputKeyboard.Key.esc:
-        # Stop listener
-        return False
-
-# Global variable to track the state of the Alt key
-alt_pressed = False
-
-# Collect events until released
-with pynputKeyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+keyboard.add_hotkey('alt+o', tkinterGUI.run)
+print('Listening for alt+o hotkey...')
+keyboard.wait()
